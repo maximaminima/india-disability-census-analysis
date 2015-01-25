6d15913ecb7fd9adf66a2665012e2cf72c9386e5
Bharat Shetty Barkur <bharat.shetty@gmail.com>
2015-01-25 09:17:03 +0530

    Getting various disabled categories vectors

--- dcensus_analysis.py
+++ dcensus_analysis.py
@@ -18,23 +18,28 @@ header1[8] = "AL"
 header1[9] = "HHI"
 header1[10] = "OW"
 
+disCat = []
 with open("total_india_stats.csv", "wb") as ds:
 	writer = csv.writer(ds, delimiter=',')
 	writer.writerow(header1)
 
 	for row in reader:
 		state = row[2]
+		disType = row[3]
 		if state == "INDIA":
 			writer.writerow(row)
+		if disType != "Total disabled population":
+			if disType not in disCat:
+				disCat.append(disType)
+# get the vector of disability categories 
+print disCat
 
-/// get the vector of disability categories 
+# for rural populace get the magnitude of each disability for male
+# for rural populace get the magnitude of each disability for female
+# a bar graph plot
 
-/// for rural populace get the magnitude of each disability for male
-/// for rural populace get the magnitude of each disability for female
-/// a bar graph plot
+# for urban populace get the magitude of each disability for male
+# for urban populace get the magitude of each disability for female
+# a bar graph plot
 
-/// for urban populace get the magitude of each disability for male
-/// for urban populace get the magitude of each disability for female
-/// a bar graph plot
-
-/// analyse the co-relation between hearing and speech categorisation
+# analyse the co-relation between hearing and speech categorisation
